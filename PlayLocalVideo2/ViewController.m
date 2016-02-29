9297c254ed074fed9af7610131195c501a80b875
thanksdanny <hj-07@163.com>
2016-02-29 23:04:59 +0800

    实现视频播放，终于解决avplayerviewController为空的问题

--- PlayLocalVideo2/ViewController.m
+++ PlayLocalVideo2/ViewController.m
@@ -9,18 +9,52 @@
 #import "ViewController.h"
 #import "VideoCell.h"
 #import <AVFoundation/AVFoundation.h>
+#import <AVKit/AVKit.h>
 
 
 @interface ViewController () <UITableViewDataSource>
-
+//{
+//    NSString *docPath;
+//    //    docPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+//
+//}
 @property (weak, nonatomic) IBOutlet UITableView *videoTableView;
+//@property (nonatomic, strong) AVPlayerViewController *playViewController;
+@property (nonatomic, strong) AVPlayer *playerView;
 
 @end
 
 @implementation ViewController
+
+//- (AVPlayer *)playerView {
+//    if (!_playerView) {
+//        AVPlayerItem *playerItem = [self getPlayItem];
+//        _playerView = [[AVPlayer alloc] initWithPlayerItem:playerItem];
+//    }
+//    return _playerView;
+//}
+//
+//- (AVPlayerItem *)getPlayItem {
+//    NSString *cachePath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+//    NSString *savePath = [cachePath stringByAppendingPathComponent:@"MOVIEPATH"]; // 示例没有加 @“”
+//    NSURL *saverUrl = [NSURL fileURLWithPath:savePath];
+//    
+//    AVPlayerItem *playerItem = [[AVPlayerItem alloc] initWithURL:saverUrl];
+//    
+//    return playerItem;
+//}
+
 - (IBAction)playBtn {
-    
-    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"emoji zone" ofType:@"mp4"];
+    NSLog(@"%@", path);
+    NSURL *videoUrl = [NSURL fileURLWithPath:path];
+    self.playerView = [[AVPlayer alloc] initWithURL:videoUrl];
+    AVPlayerViewController *videoVC = [[AVPlayerViewController alloc] init];
+    videoVC.player = self.playerView;
+    [self presentViewController:videoVC animated:YES completion:^{
+        [videoVC.player play];
+        NSLog(@"haha");
+    }];
     NSLog(@"%s", __func__);
 }
 
